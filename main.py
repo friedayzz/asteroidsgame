0df7fa6cbf37d6c33088850fb4a6d453d8dee507
Daniel Carlgren <hif_daniel@hotmail.com>
2025-03-25 17:33:55 +0100

    Rendering player(triangle) on screen

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import pygame
-
 from constants import *
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+from player import Player
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print("Screen width: 1280")
-    print("Screen height: 720")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
     
     while (True):
-        screen.fill(color=0)
-        pygame.display.flip()
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill(color=0)  
+        player.draw(screen)
+        pygame.display.flip()
+            
+        #Limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
